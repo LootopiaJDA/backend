b983d42c5e7e866003abfe55f0c22878d1f94901
Jimmy <jimmy.carlier.mma@gmail.com>
2026-01-09 14:25:35 +0100

    SCRUM-87 Suppression étape + cloud

--- src/dto/etape.dto.ts
+++ src/dto/etape.dto.ts
@@ -1,26 +1,49 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsBase64, IsNumber, IsString } from 'class-validator';
 
 
 export class EtapeDto {
   @ApiProperty({ example: 'Indice 1' })
+  @IsString()
+  @Type(() => String)
   name: string;
 
   @ApiProperty({ example: `41°24'12.2"N 2°10'26.5"E` })
+  @IsString()
+   @Type(() => String)
   lat: string;
 
   @ApiProperty({ example: `41°24'12.2"N 2°10'26.5"E` })
-  long: number;
+  @IsString()
+  @Type(() => String)
+  long: string;
 
   @ApiProperty({ example: '3 rue de la libertée'})
+  @IsString()
+  @Type(() => String)
   address: string
 
   @ApiProperty({example: 'Je suis bleu et orange' })
+  @IsString()
+  @Type(() => String)
   description: string
 
   @ApiProperty({example: 35})
-  rayon: string
+  @IsNumber()
+  @Type(() => Number)
+  rayon: number
 
   @ApiProperty({example: '3'})
+  @IsNumber()
+  @Type(() => Number)
   rank: number
+
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    description: 'Image de l\'étape'
+  })
+  image: any
 }
 
